f244c7c36a1de79a4dc65a63798fad8a2782f949
Patrick Schmitt <patrick1.schmitt@tum.de>
2024-03-04 12:39:08 +0100

    initial setup done

--- coaching_dashboard/frontend/src/App.js
+++ coaching_dashboard/frontend/src/App.js
@@ -1,35 +1,17 @@
-import React from 'react';
-import {createTheme, ThemeProvider} from "@mui/material/styles";
-import { Routes, Route } from "react-router-dom";
-import Dashboard from './components/Dashboard/Dashboard.js';
-import Checkups from './components/Checkups/Checkups.js';
 
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: `rgb(${1}, ${113}, ${189})`,
-        },
-        secondary: {
-            main: `rgb(${195}, ${195}, ${195})`,
-        },
-        tertiary: {
-            main: `rgb(${123}, ${159}, ${199})`,
-        },
-    },
-});
+import './App.css';
+import HomeTest from "./components/HomeTest";
+import {Routes, Route} from "react-router-dom";
 
-function App(){
-
-    return (
-        <>
-         <ThemeProvider theme={theme}>
-            <Routes>
-                <Route path="/" element= {<Dashboard/>}></Route>
-                <Route path="/chekups" element={<Checkups/>}></Route>
-            </Routes>
-            </ThemeProvider>
-        </>
-    )
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<HomeTest/>}></Route>
+    </Routes>
+    
+    </>
+  );
 }
 
 export default App;
---
+++ coaching_dashboard/frontend/src/components/HomeTest.js
@@ -0,0 +1,11 @@
+function HomeTest (){
+    return (
+        <div>
+            <h1>
+                Okay test test 
+            </h1>
+        </div>
+    );
+}
+
+export default HomeTest
